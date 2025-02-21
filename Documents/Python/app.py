45c97147a5b28a20ebfc217eb99470304d17803d
Ryota Okamoto <albumokamoto@gmail.com>
2025-02-21 03:28:10 +0100

    Fix UTF-8 settings for Render

--- Documents/Python/app.py
+++ Documents/Python/app.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
-import sys
 import fitz  # PyMuPDF
 from flask import Flask, request, send_file, render_template
-
-# ✅ 環境変数を UTF-8 に設定
-os.environ["PYTHONIOENCODING"] = "utf-8"
-sys.stdout.reconfigure(encoding="utf-8")
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
